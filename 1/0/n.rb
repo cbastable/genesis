7649e1be5f1b3157bc036de643541ceb09b61205
cbastable <therightstuff17@gmail.com>
2013-08-06 22:24:00 -0400

    model updates

--- 1/0/n.rb
+++ 1/0/n.rb
@@ -8,93 +8,17 @@ parent = File.expand_path("..",File.dirname(__FILE__))
 home_dir = Dir.pwd
 local_fname = "#{location}/n.txt"
 
-#check/make parent RAM
-if File.exists?("#{parent}/ram.txt")
-	previous = File.open("#{parent}/ram.txt", 'r') { |f| f.read }
-	concat = current.to_s + previous.to_s
-	#if there was previous entry in RAM, check/make new sequence
-	if !Dir.exists?("#{home_dir}/#{next_num.to_s}/#{concat}")
-		Dir.mkdir("#{home_dir}/#{next_num.to_s}")
-		Dir.mkdir("#{home_dir}/#{next_num.to_s}/#{concat}")
-		s = File.open("#{location}/n.rb", 'r') { |f| f.read }
-		File.open("#{home_dir}/#{next_num.to_s}/#{concat}/n.rb", 'w') { |f| f.write(s) }
-		# make backward & forward links from here to location & previous, update RAM
-		if File.exists?("#{location}/>.txt")
-			forward = File.readlines("#{location}/>.txt") 
-			File.open("#{location}/>.txt", 'w+') { |f| f.write("#{home_dir}/#{next_num.to_s}/#{concat}\n") } unless forward.include?(concat)
-		end
-		File.open("#{home_dir}/#{next_num.to_s}/#{concat}/<.txt", 'w+') { |f| f.write("#{home_dir}/#{number.to_s}/#{previous}") }
-		File.open("#{home_dir}/#{next_num.to_s}/#{concat}/<.txt", 'w+') { |f| f.write("#{home_dir}/#{number.to_s}/#{current}") }
-		File.open("#{parent}/ram.txt", 'w') { |f| f.write("#{current}.to_s") }
-		Dir.glob("#{home_dir}/#{next_num.to_s}/#{concat}/n.rb") do |p| #can I kill this safely?
-			begin
-			  PTY.spawn( "ruby #{p}" ) do |stdout, stdin, pid|
-			    begin
-			    	stdout.each { |line| puts line }
-			    rescue Errno::EIO
-			    end
-			  end
-			rescue PTY::ChildExited
-			  puts "The child process exited!"
-			end
-		end
-	else #this sequence already exists so just go there, fire it, & update
-		File.open("#{parent}/ram.txt", 'w') { |f| f.write("#{current}.to_s") }
-		Dir.glob("#{home_dir}/#{next_num.to_s}/#{concat}/n.rb") do |p| #can I kill this safely?
-			begin
-			  PTY.spawn( "ruby #{p}" ) do |stdout, stdin, pid|
-			    begin
-			    	stdout.each { |line| puts line }
-			    rescue Errno::EIO
-			    end
-			  end
-			rescue PTY::ChildExited
-			  puts "The child process exited!"
-			end
-		end
-	end
-else
-	File.open("#{parent}/ram.txt", 'w') { |f| f.write("#{location}".to_s) }
-	#read next input? predictions?
-	Dir.glob("#{home_dir}/c.rb") do |p|
-		begin
-		  PTY.spawn( "ruby #{p} #{location}" ) do |stdout, stdin, pid| #arguments?
-		    begin
-		    	stdout.each { |line| puts line }
-		    rescue Errno::EIO
-		    end
-		  end
-		rescue PTY::ChildExited
-		  puts "The child process exited!"
-		end
-	end
-end
-
-#bottom-up prediction (put this above in the else statement?)
 forward_connections = []
 File.open("#{location}/>.txt", 'r').each_line { |line| forward_connections << line}
-forward_connections.each do |connection| 
-	Dir.glob("#{connection}/n.rb") do |p|
-		begin
-		  PTY.spawn( "ruby #{p} f" ) do |stdout, stdin, pid| #arguments?
-		    begin
-		    	stdout.each { |line| puts line }
-		    rescue Errno::EIO
-		    end
-		  end
-		rescue PTY::ChildExited
-		  puts "The child process exited!"
-		end
-	end
-end
-
-#top-down prediction (put this where? else statement?)
 backward_connections = []
-File.open("#{location}/<.txt", 'r').each_line { |line| forward_connections << line}
-backward_connections.each do |connection|
-	Dir.glob("#{connection}/n.rb") do |p|
+File.open("#{location}/<.txt", 'r').each_line { |line| backward_connections << line}
+global_ram = File.open("#{home_dir}/ram.txt", 'r') { |f| f.read }
+
+#if this neuron is being 100% predicted, but this neuron also only has one possible outcome, keep going
+if ARGV[0] == "predict_up" && forward_connections.size < 2 && forward_connections.first.size > 0
+	Dir.glob("#{home_dir}/#{forward_connections.first}") do |p|
 		begin
-		  PTY.spawn( "ruby #{p} b" ) do |stdout, stdin, pid| #arguments?
+		  PTY.spawn( "ruby #{p} predict_up" ) do |stdout, stdin, pid| #arguments?
 		    begin
 		    	stdout.each { |line| puts line }
 		    rescue Errno::EIO
@@ -104,7 +28,125 @@ backward_connections.each do |connection|
 		  puts "The child process exited!"
 		end
 	end
-end
+#no predictions?
+elsif ARGV[0] == "predict_up" && forward_connections.size <= 1 && forward_connections.first.size < 1
+	Dir.glob("#{home_dir}/c.rb") do |p|
+		begin
+		  PTY.spawn( "ruby #{p} #{number}" ) do |stdout, stdin, pid| #arguments?
+		    begin
+		    	stdout.each { |line| puts line }
+		    rescue Errno::EIO
+		    end
+		  end
+		rescue PTY::ChildExited
+		  puts "The child process exited!"
+		end
+	end
+#elsif [multiple predictions] stop and match what it predicts vs. the actual next inputs
+elsif ARGV[0] == "predict_up" && forward_connections.size >= 2
+	input_size = global_ram.partition('/').last.to_i
+	prediction_index = backward_connections.index("#{global_ram}") + 1
+	prediction = backward_connections[prediction_index]
+	Dir.glob("#{home_dir}/#{prediction}") do |p|
+		begin
+		  PTY.spawn( "ruby #{p} predict_down" ) do |stdout, stdin, pid|
+		    begin
+		    	stdout.each { |line| puts line }
+		    rescue Errno::EIO
+		    end
+		  end
+		rescue PTY::ChildExited
+		  puts "The child process exited!"
+		end
+	end
+elsif ARGV[0] == "predict_down" #predict this to happen, read next number of inputs to verify
+	#CHANGE THIS -- SHOULD NOT GET NEXT NUMBER HERE, get it in event-fire location instead plz
+	File.open("#{home_dir}/ram.txt", 'w') { |f| f.write("#{location}.to_s") }
+	Dir.glob("#{home_dir}/c.rb") do |p|
+	begin
+	  PTY.spawn( "ruby #{p} #{number}" ) do |stdout, stdin, pid| #arguments?
+	    begin
+	    	stdout.each { |line| puts line }
+	    rescue Errno::EIO
+	    end
+	  end
+	rescue PTY::ChildExited
+	  puts "The child process exited!"
+	end
+	end
+elsif ARGV[0] == "input"
+	sequence_size = (global_ram.partition('/').last.partition('/').first.to_i)
+	next_sequence_size = sequence_size + number
+	next_sequence_size = 1 if next_sequence_size == 0
+	already_created = 0
+	#neurons this one is connected to:
+	hash = Hash.new
+	forward_connections.each do |connection|
+		links = []
+		connection_size = connection.partition('/').last.partition('/').first.to_i
+		if connection_size == next_sequence_size
+			File.open("#{home_dir}/#{connection}/<.txt", 'r').each_line { |line| links << line}
+		end
+		hash['connection'] = links
+	end
+	indices_hash = Hash.new
+	hash.each do |connection, backlinks|
+		#link_index = value.index(location)
+		#index_hash = { "#{key}" => link_index }
+		indices = backlinks.each_index.select{ |i| backlinks[i] == location }
+		indices_hash["#{connection}"] = indices
+	end
+	indices_hash.each do |connection, indices|
+		possiblities = hash[connection]
+		indices.each do |index|
+			if possiblities[(index - 1)] == global_ram #already learned this, go fire it
+				already_created = 1
+				Dir.glob("#{home_dir}/#{connection}") do |p|
+					begin
+					  PTY.spawn( "ruby #{p} input" ) do |stdout, stdin, pid|
+					    begin
+					    	stdout.each { |line| puts line }
+					    rescue Errno::EIO
+					    end
+					  end
+					rescue PTY::ChildExited
+					  puts "The child process exited!"
+					end
+				end #dir.glob
+			end # if possibilities
+		end
+	end
+	if already_created == 0 #new sequence, learn it
+		num = 0 
+	 	Dir.glob("#{home_dir}/#{next_sequence_size.to_s}/*").select do |f| 
+	 		File.directory?(f)
+	 		num = num + 1
+	 	end
+		Dir.mkdir("#{home_dir}/#{next_sequence_size.to_s}") unless Dir.exists?("#{home_dir}/#{next_sequence_size.to_s}")
+		Dir.mkdir("#{home_dir}/#{next_sequence_size.to_s}/#{num.to_s}")
+		s = File.open("#{location}/n.rb", 'r') { |f| f.read }
+		File.open("#{home_dir}/#{next_sequence_size.to_s}/#{num.to_s}/n.rb", 'w') { |f| f.write(s) }
+		File.open("#{home_dir}/#{next_sequence_size.to_s}/#{num.to_s}/>.txt", 'w') { |f| f.write("") }
+		File.open("#{home_dir}/#{next_sequence_size.to_s}/#{num.to_s}/<.txt", 'w') { |f| f.write("") }
+		File.open("#{location}/>.txt", 'w+') { |f| f.write("#{home_dir}/#{next_sequence_size.to_s}/#{num.to_s}\n") }
+		File.open("#{home_dir}/#{next_sequence_size.to_s}/#{num.to_s}/<.txt", 'w+') { |f| f.write("#{global_ram}\n") }
+		File.open("#{home_dir}/#{next_sequence_size.to_s}/#{num.to_s}/<.txt", 'w+') { |f| f.write("#{location}\n") }
+		#File.open("#{home_dir}/ram.txt", 'w') { |f| f.write() }
+	end
+else
+
+end #BIG if
+
+#if only one forward connection, go there, repeat until multiple predictions or no more predictions
+#check it against input with .include?, if wrong, read input with c.rb to learn
+
+
+
+#if multiple forward connections, but one of those forward connections is being activated from above, go there, check it 
+#against input with .include?, if wrong, read input with c.rb to learn
+
+
+# If I already know what I see next - don't remember it in ram, keep building sequence until limit of known is reached
 # if a neuron fires and WAS NOT being predicted, that is an event worthy of attention (re-scan input to learn sequence?)
 # if a neuron fires and WAS being predicted, that sequence needs to be strengthened in memory
 # MAKE all future predictions ( -- sub-conscious-thought -- ) but don't take any ACTION until the # of predictions has been narrowed
--- c.rb
+++ c.rb
@@ -1,39 +1,72 @@
 require 'pty'
 location = "#{File.dirname(__FILE__)}"
 size = 1
-if ARGV[0] == "external"
+pos = 0
+feed = ARGV[0]
+if feed == "external"
 	print "Conrad: "
 	input = $stdin.gets.chomp.unpack('B*')
 	File.open("#{location}/c.txt", "w") { |f| f.write(input.first)} #.unpack returns array, so .first is just the input?
-	curr_char = input[0][0].to_s
+	curr_char = input[pos][0].to_s
 	#make a directory for curr_char if doesn't exist...
-	if !Dir.exists?("#{location}/#{size}/#{curr_char}")
-		Dir.mkdir("#{location}/#{size}")
-		Dir.mkdir("#{location}/#{size}/#{curr_char}")
-		s = File.open("#{location}/n.rb", 'r') { |f| f.read }
-		File.open("#{location}/#{size}/#{curr_char}/n.rb", 'w') { |f| f.write(s) }
-	end
-	#make ram for current input/write to ram here
-	#go to curr_char, fire neuron
-	Dir.glob("#{location}/#{size}/#{curr_char}/n.rb") do |p|
-		begin
-		  PTY.spawn( "ruby #{p} upwards" ) do |stdout, stdin, pid|
-		    begin
-		    	stdout.each { |line| puts line }
-		    rescue Errno::EIO
-		    end
-		  end
-		rescue PTY::ChildExited
-		  puts "The child process exited!"
+	if curr_char == 0.to_s
+		if !Dir.exists?("#{location}/#{size}/0")
+			Dir.mkdir("#{location}/#{size}")
+			Dir.mkdir("#{location}/#{size}/0")
+			s = File.open("#{location}/n.rb", 'r') { |f| f.read }
+			File.open("#{location}/#{size}/0/n.rb", 'w') { |f| f.write(s) }
+			File.open("#{location}/#{size}/0/>.txt", 'w') { |f| f.write("") }
+			File.open("#{location}/#{size}/0/<.txt", 'w') { |f| f.write("") }
 		end
-	end
+		#make ram for current input/write to ram here
+		#go to curr_char, fire neuron
+		pos = 1
+		File.open("#{location}/pos.txt", 'w') { |f| f.write(pos) }
+		File.open("#{location}/ram.txt", 'w') { |f| f.write("#{location}/#{size}/0") }
+		Dir.glob("#{location}/#{size}/0/n.rb") do |p|
+			begin
+			  PTY.spawn( "ruby #{p} input" ) do |stdout, stdin, pid|
+			    begin
+			    	stdout.each { |line| puts line }
+			    rescue Errno::EIO
+			    end
+			  end
+			rescue PTY::ChildExited
+			  puts "The child process exited!"
+			end
+		end
+	else
+		if !Dir.exists?("#{location}/#{size}/1")
+			Dir.mkdir("#{location}/#{size}")
+			Dir.mkdir("#{location}/#{size}/1")
+			s = File.open("#{location}/n.rb", 'r') { |f| f.read }
+			File.open("#{location}/#{size}/1/n.rb", 'w') { |f| f.write(s) }
+			File.open("#{location}/#{size}/1/>.txt", 'w') { |f| f.write("") }
+			File.open("#{location}/#{size}/1/<.txt", 'w') { |f| f.write("") }
+		end
+		#make ram for current input/write to ram here
+		#go to curr_char, fire neuron
+		pos = 1
+		File.open("#{location}/pos.txt", 'w') { |f| f.write(pos) }
+		File.open("#{location}/ram.txt", 'w') { |f| f.write("#{location}/#{size}/1") }
+		Dir.glob("#{location}/#{size}/1/n.rb") do |p|
+			begin
+			  PTY.spawn( "ruby #{p} input" ) do |stdout, stdin, pid|
+			    begin
+			    	stdout.each { |line| puts line }
+			    rescue Errno::EIO
+			    end
+			  end
+			rescue PTY::ChildExited
+			  puts "The child process exited!"
+			end
+		end
+	end	
 else
 	#need smart way to find out where in the input I currently am/was reading from
-	string = ARGV[0]
-	puts string
+	pos = File.open('#{location}/pos.txt', 'r') { |f| f.read }
 	input = File.open('#{location}/c.txt', 'r') { |f| f.read }
-	input[0] = ""
-	File.open("#{location}/c.txt", "w") { |f| f.write(input)}
+	substring = input[pos..(pos + feed - 1)]
 	curr_char = input[0].to_s
 	if !Dir.exists?("#{location}/#{size}/#{curr_char}")
 		Dir.mkdir("#{location}/#{size}")
